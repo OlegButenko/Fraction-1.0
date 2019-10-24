8b1dc14099990c846d108483aaebd8a77b4c3463
Oleg <oleggodofthunderbutenko@gmail.com>
2019-10-24 03:01:35 +0300

    Second commit

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,2 +1,22 @@
 public class Fraction {
+
+    private int numerator;
+    private int denominator;
+
+    public Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+    public void printFraction() {
+        System.out.println(getNumerator() + "/" + getDenominator());
+    }
 }
