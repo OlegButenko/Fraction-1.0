dd5ee66eeddc8bfc5cb0fe2ea7346952830ad77f
Oleg <oleggodofthunderbutenko@gmail.com>
2019-10-24 03:13:52 +0300

    4th commit

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,12 @@ public class Main {
 
         Fraction a = new Fraction(1,1);
         a.printFraction();
+
+        Fraction b = new Fraction(2,2);
+        Fraction c = new Fraction(3,3);
+        Fraction d = b.multiplication(c);
+        d.printFraction();
+
     }
 
 }
