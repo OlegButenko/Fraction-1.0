874c4717be9ec0134e6d7a56ed6536bf34be52ee
Oleg <oleggodofthunderbutenko@gmail.com>
2019-10-25 14:43:54 +0300

    6th commit

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,11 +1,33 @@
-public class Fraction {
+import static java.lang.Math.abs;
+
+class Fraction {
 
     private int numerator;
     private int denominator;
 
-    public Fraction(int numerator, int denominator) {
-        this.numerator = numerator;
-        this.denominator = denominator;
+    Fraction(int numerator, int denominator) {
+        if (denominator == 0) {
+            System.out.println("denominator can't be equal 0");
+        }
+        else if (numerator == 0) {
+            this.numerator = 0;
+            this.denominator = 0;
+        }
+        else if (numerator == denominator) {
+            this.numerator = 1;
+            this.denominator = 1;
+        }
+        else if (numerator < 0 && denominator < 0) {
+            this.numerator = -1 * numerator;
+            this.denominator = -1 * denominator;
+        }
+        else {
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+        int x = FindGCD(abs(this.numerator), abs(this.denominator));
+        this.numerator /= x;
+        this.denominator /= x;
     }
 
     public int getNumerator() {
@@ -16,12 +38,30 @@ public class Fraction {
         return denominator;
     }
 
+    public void setDenominator(int denominator) {
+        this.denominator = denominator;
+    }
+
     public void printFraction() {
         System.out.println(getNumerator() + "/" + getDenominator());
     }
 
-    public Fraction multiplication(Fraction other) {
-        Fraction a = new Fraction(this.numerator * other.getNumerator(), this.denominator * other.getDenominator());
+    public int FindGCD(int a, int b) {
+        while (b !=0) {
+            int tmp = a % b;
+            a = b;
+            b = tmp;
+        }
         return a;
+    }
+
+    public void reduction(int a, int b) {
+        int x = FindGCD(a,b);
+        this.numerator /= x;
+        this.denominator /= x;
+    }
+
+    public Fraction multiplication(Fraction other) {
+        return new Fraction(this.numerator * other.getNumerator(), this.denominator * other.getDenominator());
     }
 }
